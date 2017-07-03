b7f9d01bb1a0d2c1ce12b490e67ad798d6561a86
Ted Ian Osias <osiastedian@gmail.com>
2017-07-03 08:05:39 +0800

    Added Modal for Login

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router'
 import { AppComponent} from './app.component'
 import { AboutComponent } from './about/about.component'
 import { ContactComponent } from './contact/contact.component'
-import { LoginComponent } from './login/login.component'
 import { SermonsComponent } from './sermons/sermons.component'
 import { HomeComponent } from './home/home.component'
 
@@ -12,7 +11,6 @@ export const rounter : Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'sermons', component: SermonsComponent},
-    {path: 'login', component: LoginComponent},
     {path: 'contact', component: ContactComponent}
 ];
 
