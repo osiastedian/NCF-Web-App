e5b977a36394286aab79a94518bdcda32c74c0d0
Ted Ian Osias <osiastedian@gmail.com>
2017-07-03 23:24:43 +0800

    Start Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Northview Christian Fellowship';
+  title = 'Northview Christian Fellowship';  
 }
